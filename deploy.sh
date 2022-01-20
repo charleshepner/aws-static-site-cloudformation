167a44d625bde2a05f425aefd8f0871f29c1b05d
Charles Hepner <charleshepner@gmail.com>
2022-01-20 00:00:37 -0500

    add accountid to bucket name and pass role to cloudformation

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-CLOUDFORMATTION_FILE=static-site.yml
-
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+CLOUDFORMATION_FILE=static-site.yml
 
 echo "==> Validating ${CLOUDFORMATION_FILE} ..."
 aws cloudformation validate-template --template-body file://$(pwd)/${CLOUDFORMATION_FILE}
 
 echo "==> Deploying ${CLOUDFORMATION_FILE} ..."
-aws cloudformation deploy --stack-name "static-site-cf" --template-file ${CLOUDFORMATION_FILE}
+aws cloudformation deploy --stack-name "static-site-cf" --template-file ${CLOUDFORMATION_FILE} --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/cloudformation"
