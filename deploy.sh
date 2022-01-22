96cb44d86df76e8ff3be8dc0c08155d2302e5e4b
Charles Hepner <charleshepner@gmail.com>
2022-01-21 22:47:06 -0500

    Create DNS name based on the first two argumetns to the script (subdomain and domain)

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 CLOUDFORMATION_FILE=static-site.yml
 
 echo "==> Validating ${CLOUDFORMATION_FILE} ..."
-aws cloudformation validate-template --template-body file://$(pwd)/${CLOUDFORMATION_FILE}
+aws cloudformation validate-template \
+  --template-body file://$(pwd)/${CLOUDFORMATION_FILE}
 
 echo "==> Deploying ${CLOUDFORMATION_FILE} ..."
-aws cloudformation deploy --stack-name "static-site-cf" --template-file ${CLOUDFORMATION_FILE} --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/cloudformation"
+aws cloudformation deploy \
+  --stack-name "static-site-cf" \
+  --template-file ${CLOUDFORMATION_FILE} \
+  --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/cloudformation" \
+  --parameter-overrides "SubdomainName=$1" "DomainName=$2"
